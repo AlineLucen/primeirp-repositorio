82dbab058952267000e06b6b38715a4b5dfc4e85
Aline Rafaela <alineadslucena@gmail.com>
2022-09-15 21:39:52 -0300

    primeiro repositorio de js

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Tipos primotivos
+ *  Variaveis no js
+ * operadores matematicos
+ * 
+ */
+// string -> texto
+// number -> números
+// boolean -> true/false
+
+console.log ("hello world!")
+console.log ( 43.76)
+
+
+// variáveis
