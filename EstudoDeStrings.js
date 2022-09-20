9c6de5ddaff69a39cf16e13024368d86cbdd15cc
Aline Rafaela <alineadslucena@gmail.com>
2022-09-20 15:30:02 -0300

    strings e concatenação

---
+++ EstudoDeStrings.js
@@ -0,0 +1,14 @@
+//delimitação de string
+
+const aspasSimples = 'olá mundo'
+console.log(aspasSimples)
+
+//caracteres de escapa \n = quebra linha]
+
+const aspasDuplas = " outra mensagem\n tudo bem"
+console.log(aspasDuplas)
+ const textoComCrase = `olá eu sou aline 
+  estou bem obrigda`
+  console.log(textoComCrase)
+
+
